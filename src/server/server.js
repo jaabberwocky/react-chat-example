c90476e171ad14590b69f4431076bf4eddccd1d7
Tobias Leong <tobias.leong@zendesk.com>
2022-01-29 18:14:34 +0800

    Handle scheduled messages from server

--- src/server/server.js
+++ src/server/server.js
@@ -13,13 +13,30 @@ const io = new Server(httpServer, {
 const PORT = 3001;
 
 // "DB" for now
-let msg = [{ id: 0, user: "john", text: "hello world" }];
+let msg = [{ id: 0, user: "john", text: "hello world", time: Date.now() }];
+
+const addNewMsg = (socket, id) => {
+  console.log(`Pushing new msg with id ${id}...`);
+  msg.push({
+    id: id,
+    user: "toby",
+    text: "hey man!",
+    time: Date.now(),
+  });
+  socket.emit("get:msg", { msg: msg });
+};
 
 io.on("connection", (socket) => {
   console.log(`connection established: ${socket.id}`);
   console.log("Sending message...");
 
   socket.emit("get:msg", { msg: msg });
+
+  let i = 1;
+  while (i < 10) {
+    setTimeout(addNewMsg, 1000 + (i * 1000), socket, i);
+    i = i + 1;
+  }
 });
 
 console.log(`starting server on port ${PORT}...`);
